85e551b23050477ccc87feedfafb2f33b2f1f82d
Sabari M <151687270+sabarim6369@users.noreply.github.com>
2025-05-18 10:30:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require("express")
 const app=express();
 app.get("/",(req,res)=>{
-    res.send("Byeff");
+    res.send("Byef");
 })
 app.listen(8000,'0.0.0.0',()=>{
 console.log("Server running in port 8000")
