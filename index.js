4cb7b7ea84ac05082c9cd1f9651e5efec3d54548
Sabari M <151687270+sabarim6369@users.noreply.github.com>
2025-05-18 11:14:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require("express")
 const app=express();
 app.get("/",(req,res)=>{
-    res.send("Byewrld😎😎😎😎😎😎");
+    res.send("Byewrld");
 })
 app.listen(8000,'0.0.0.0',()=>{
 console.log("Server running in port 8000")
